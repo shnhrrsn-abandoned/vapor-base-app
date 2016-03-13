ae6defb416e6c5e629c6c46c7c63eb334a8c8cc1
shnhrrsn <shnhrrsn@gmail.com>
2016-03-12 23:38:36 -0500

    Fixed StencilProvider to work with latest Vapor changes

--- App/Providers/StencilProvider.swift
+++ App/Providers/StencilProvider.swift
@@ -6,19 +6,12 @@ import PathKit
 class StencilProvider: Vapor.Provider {
 
 	static func boot(application: Application) {
-		View.renderers[".stencil"] = StencilRenderer()
-	}
+		if View.renderers[".stencil"] != nil {
+			return
+		}
 
-}
-
-private class StencilRenderer: VaporStencil.StencilRenderer {
-
-	override func render(template template: String, context: [String: Any]) throws -> String {
-		let c = Context(dictionary: context)
-		c["loader"] = TemplateLoader(paths: [Path(View.resourceDir + "/views")])
-
-		let template = Template(templateString: template)
-		return try template.render(c)
+		let renderer = StencilRenderer(templateLoader: TemplateLoader(paths: [Path(View.resourceDir + "/views")]))
+		View.renderers[".stencil"] = renderer
 	}
 
 }
